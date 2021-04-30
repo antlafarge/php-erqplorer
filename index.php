fda7e706ebf878dcda85788a90d57ebed8f3c9ee
Antoine Lafarge <ant.lafarge@gmail.com>
2021-04-30 23:06:28 +0200

    v0.4

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 
+// Commande de sï¿½curitï¿½
+define ("INC", 1);
+
+require_once ('./explorateur_ftp/fonctions.php');
+
 // CHANGEMENT DU STYLE
-if (isset($_GET['style']) AND $_GET['style'] != NULL) { // Si un changement de style doit etre effectué
+if (isset($_GET['style']) AND !empty ($_GET['style'])) { // Si un changement de style doit etre effectuï¿½
 	setcookie ('style', $_GET['style'], time()+365*3600);
 	$style = $_GET['style'];
 }
-elseif (isset($_COOKIE['style']) AND $_COOKIE['style'] != NULL) // Si un cookie de changement de style est trouvé
+elseif (isset($_COOKIE['style']) AND !empty ($_COOKIE['style'])) // Si un cookie de changement de style est trouvï¿½
 	$style = $_COOKIE['style'];
-else // Style par défaut
+else // Style par dï¿½faut
 	$style = 'bleu';
+
+// Traitement de l'adresse ï¿½ explorer
+if (!isset($_GET['url']) || empty($_GET['url']) || strpos($_GET['url'],'../') !== FALSE || strpos($_GET['url'],'./') === FALSE)
+	$_GET['url'] = './';
+elseif (!preg_match ('!/$!', $_GET['url']))
+	$_GET['url'] = $_GET['url'].'/';
 
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
@@ -19,137 +30,63 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.or
 	<meta name="description" content="Explorateur FTP" />
 	<meta name="generator" content="Notepad++ v4.0.2" />
 	<meta name="keywords" content="Explorateur, FTP, '.$_SERVER["HTTP_HOST"].'" />
-	<link rel="stylesheet" media="screen" type="text/css" title="Actuel" href="explorateur_ftp/styles/ftp_'.$style.'.css" />
-	<link rel="shortcut icon" href="explorateur_ftp/icones/ftp_'.$style.'.png" />
+	<link rel="stylesheet" media="screen" type="text/css" title="Actuel" href="./explorateur_ftp/styles/'.$style.'.css" />
+	<link rel="shortcut icon" href="./explorateur_ftp/icones/icone.png" />
 	<title>'.$_SERVER["HTTP_HOST"].'</title>
 </head>
 <body>';
 
-// Fonction récursive permettant l'affichage de l'arborescence
-function lister_dossiers ($dossier) {
-	if ($dossier_ouvert = opendir ($dossier)) {
-		// Création du tableau contenant la liste des dossiers
-		$liste_dossiers = array ();
-		while (false !== ($entree = readdir ($dossier_ouvert)))
-			$liste_dossiers[] = $entree;
-		closedir ($dossier_ouvert);
-		sort ($liste_dossiers);
-		$count = count ($liste_dossiers);
-		// Affichage du tableau contenant la liste des dossiers
-		for ($nb = 0; $nb <= $count ; $nb++) {
-			$dossier_url = $dossier.$liste_dossiers[$nb].'/';
-			if (@is_dir ($dossier_url) AND $liste_dossiers[$nb] != '.' AND $liste_dossiers[$nb] != '..' AND $liste_dossiers[$nb] != 'explorateur_ftp' AND $liste_dossiers[$nb] != NULL) {
-				echo '<tr><td>';
-				// Gestion de la profondeur des alinéas
-				$profondeur = preg_match_all ('!/!', $dossier_url, $tab) - 1;
-				for ($i = 0 ; $i < 4*$profondeur ; $i++) {
-					echo '&nbsp;';
-				}
-				if (!preg_match ("!^$dossier_url!i", $_GET['url']))
-					echo '<a href="./index.php?url='.$dossier_url.'"><img src="../explorateur_ftp/icones/repertoire.png" /> '.$liste_dossiers[$nb].'</a></td></tr>';
-				else {
-					echo '<a href="./index.php?url='.$dossier_url.'" class="dossier_courant"><img src="../explorateur_ftp/icones/repertoire_ouvert.png" />&nbsp;'.$liste_dossiers[$nb].'</a></td></tr>';
-					lister_dossiers ($dossier_url);
-				}
-			}
-		}
-	}
-	else
-		die ("Dossier innaccessible");
-}
-
-// Fonction Listant les fichiers du répertoire
-function lister_fichiers ($dossier) {
-	if ($dossier_ouvert = opendir ($dossier)) {
-		// Création du tableau contenant la liste des dossiers
-		$liste_fichiers = array ();
-		while (false !== ($entree = readdir ($dossier_ouvert)))
-			$liste_fichiers[] = $entree;
-		closedir ($dossier_ouvert);
-		sort ($liste_fichiers);
-		$count = count ($liste_fichiers);
-		// Affichage du tableau contenant la liste des dossiers
-		for ($nb = 0; $nb <= $count ; $nb++) {
-			$fichier_url = $dossier.$liste_fichiers[$nb];
-			if (@is_file ($fichier_url) AND !preg_match ("!^./explorateur_ftp!i", $fichier_url) AND $fichier_url != './index.php' AND $fichier_url != './phpinfo.php') {
-				// Traitement des icones
-				if (preg_match ('!.+[.](.+$)!i', $liste_fichiers[$nb], $tableau)) {
-					$extension = strtolower ($tableau['1']);
-					$url_image = './explorateur_ftp/icones_extensions/'.$extension.'.png';
-					if (!file_exists ($url_image))
-						$url_image = './explorateur_ftp/icones_extensions/inconnu.png';
-				}
-				$icone = '<img src="'.$url_image.'" /> ';
-				// Traitement de la taille la plus adaptée
-				if (!$taille = filesize ($fichier_url))
-					$taille = 'Inconnu';
-				else {
-					for ($i=0 ; ($taille/1024)>1 AND i<4 ; $i++) {
-						$taille /= 1024;
-					}
-					$type = array(' octets',' Ko',' Mo',' Go',' To');
-					// Traitement des décimales
-					$tab = explode (".",$taille);
-					if ($tab[0] != $taille)
-						$taille = number_format ($taille, 2, ',', ' ');
-					$taille = $taille.$type[$i];
-				}
-				// Traitement de la date de dernière modification
-				$date = filemtime ($fichier_url);
-				$date = date ('d/m/y - H:i:s', $date);
-				// Affichage
-				echo '<tr class="fichier"><td><a href="'.$fichier_url.'">'.$icone.$liste_fichiers[$nb].'</a></td><td>'.$taille.'</td><td>'.$date.'</td></tr>';
-			}
-		}
-	}
-	else // Si l'utilisateur entre une adresse incorrecte
-		echo '<div class="erreur">Erreur : Dossier innaccessible</div>';
-}
-
-
-// Traitement de l'adresse à explorer
-if (!isset($_GET['url']) || empty($_GET['url']) || strpos($_GET['url'],'../') !== FALSE || strpos($_GET['url'],'./') === FALSE)
-	$_GET['url'] = './';
-elseif (!preg_match ('!/$!', $_GET['url']))
-	$_GET['url'] = $_GET['url'].'/';
-	
 // Titre du site
-echo '<h1>'.$_SERVER["HTTP_HOST"].'</h1>';
-// Affichage de l'arborescence
-echo '<table id="ftp">
-	<tr><th id="adresse_barre" colspan="2">Adresse <img src="explorateur_ftp/icones/repertoire_ouvert.png" /> <form id="adresse_formulaire" method="get" action="./index.php"><input id="adresse_text" type="text" name="url" value="'.$_GET['url'].'" /> <input id="adresse_submit" type="submit" value="OK" /></form></th></tr>
-	<tr><td id="arborescence_dossiers" valign="top">
-		<table class="fenetre">
-			<tr><th class="titre">Arborescence</th></tr>
-			<tr><td><a href="index.php?url=./" class="dossier_courant"> <img src="explorateur_ftp/icones/retour.png" border="0" /> Racine</a></td></tr>';
-			lister_dossiers ('./');
-echo '	</table>
-	</td>';
+echo '<div id="banniere">'.$_SERVER["HTTP_HOST"].'</div>';
 
-// Affichage des fichiers du dossier exploré
-echo '<td id="arborescence_fichiers" valign="top">
-		<table class="fenetre">
-			<tr><th class="titre" colspan="3">Fichiers contenus dans le dossier</th></tr>
-			<tr><th class="sous_titre">Nom</th><th class="sous_titre">Taille</th>
-			<th class="sous_titre">Dernière Modification</th></tr>';
-			lister_fichiers ($_GET['url']);
-echo '	</table>
-	</td></tr>
-<tr><td id="credits" colspan="2"><> Copyright 2007 <>
-	<form method="get">
-		<input type="hidden" name="url" value="'.$_GET['url'].'">
-		<select id="design_liste_deroulante" name="style">
-			<option value="bleu" selected="selected">Design</option>
-			<option value="bleu">Bleu</option>
-			<option value="rouge">Rouge</option>
-			<option value="orange">Orange</option>
-			<option value="vert">Vert</option>
-			<option value="rose">Rose</option>
-		</select>
-		<input type="submit" id="design_submit" value="ok">
-	</form>
-<> Code : <a href="mailto:erasor@hotmail.fr" title="erasor@hotmail.fr">eRaSoR</a> !! <></td></tr>
-</table>';
+// Affichage de la barre d'adresse
+echo '
+<table id="ftp">
+	<tr>
+		<td id="adresse" colspan="2">
+			<form method="get" action="./index.php">
+				<table>
+					<tr>
+						<td id="adresse_text">Adresse <img src="./explorateur_ftp/icones/repertoire_ouvert.png" alt="[rep]" /></td>
+						<td><p><input id="adresse_barre" type="text" name="url" value="'.$_GET['url'].'" /></p></td>
+						<td id="adresse_submit"><p><input type="submit" value="OK" /></p></td>
+					</tr>
+				</table>
+			</form>
+		</td>
+	</tr>
+	
+	<tr>
+		<td id="dossiers_cadre" valign="top">
+			<table>
+				<tr><td class="titre">Arborescence</td></tr>
+				<tr><td><a href="index.php?url=./" class="dossier_courant"><img src="./explorateur_ftp/icones/retour.png" alt="[ret]" /> Racine</a></td></tr>';
+				lister_dossiers ('./');
+echo '		</table>
+		</td>
+		<td id="fichiers_cadre" valign="top">
+			<table>
+				<tr><td class="titre" colspan="5">Fichiers contenus dans le dossier</td></tr>
+				<tr><td class="sous_titre" colspan="2">Nom</td><td class="sous_titre" colspan="2">Taille</td><td class="sous_titre">Modification</td></tr>';
+				lister_fichiers ($_GET['url']);
+echo '		</table>
+		</td>
+	</tr>
+	
+	<tr>
+		<td id="credits" colspan="2">
+			PHP-ErqPlorer v0.4 - Copyright 2oo7-2oo8
+			<form id="design" method="get" action="./index.php?url='.$_GET['url'].'">
+				<p><input type="hidden" name="url" value="'.$_GET['url'].'" /></p>';
+				list_select ('css', './explorateur_ftp/styles/', 'style');
+echo '			<p><input type="submit" id="design_submit" value="ok" /></p>
+			</form>
+			W3C <a href="http://validator.w3.org/check?uri=referer">XHTML</a>, <a href="http://jigsaw.w3.org/css-validator/">CSS</a> Validated -
+			Webmaster : <a href="mailto:erasor@hotmail.fr" title="erasor@hotmail.fr">erasor</a>
+		</td>
+	</tr>
+</table>
 
-echo '</body></html>';
+</body>
+</html>';
 ?>
